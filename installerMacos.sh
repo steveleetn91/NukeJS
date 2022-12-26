e6c3f24ed0df4432abb0cc96ce6a1e06888cafae
adminhopee <hoang.le.tn91@gmail.com>
2022-12-26 16:55:19 +0700

    Update installer for MacOS

--- installerMacos.sh
+++ installerMacos.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-npm i -g cordova && npm install && npm run build && sudo chmod -R 777 ./dist && sudo npm link && npm run setup && npm run add:web && npm run build:web
+sudo npm i -g cordova && npm install && npm run build && sudo chmod -R 777 ./dist && sudo npm link && npm run setup && npm run add:web && npm run build:web
